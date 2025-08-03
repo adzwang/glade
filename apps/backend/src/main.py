0298c2cc8ccb5dad0e4df9b7914080daacedbdd3
Adam Wang <adam.wang@hotmail.co.uk>
2025-08-03 21:58:50 +0100

    add CORS middleware to whitelist origin

--- apps/backend/src/main.py
+++ apps/backend/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,4 +7,16 @@ load_dotenv()
 from src.widgets import register as register_widgets
 
 app = FastAPI(title="Glade")
+
+origins = [
+  "http://localhost:3000",
+]
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"]
+)
+
 register_widgets(app)
